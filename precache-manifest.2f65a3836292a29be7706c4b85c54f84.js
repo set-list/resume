2839ce8aaaf6682516e0e8d593bf9fe2a1a47af2
sam <samverm10@gmail.com>
2018-10-20 01:52:36 -0400

    Updates

---
+++ precache-manifest.2f65a3836292a29be7706c4b85c54f84.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "abf03e7691f3a7759896",
+    "url": "/static/js/main.abf03e76.chunk.js"
+  },
+  {
+    "revision": "4fd91c74e51506835aa9",
+    "url": "/static/js/1.4fd91c74.chunk.js"
+  },
+  {
+    "revision": "abf03e7691f3a7759896",
+    "url": "/static/css/main.8dab4cb6.chunk.css"
+  },
+  {
+    "revision": "d8b6054ab53b45006d773dae0b96af55",
+    "url": "/index.html"
+  }
+];
